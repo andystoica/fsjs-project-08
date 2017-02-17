03ce9083cd8150ad1f0e8956d5f18b2f1bd8b332
Andy Stoica <andy.stoica@me.com>
2017-02-17 13:26:00 +0000

    Added eslint and code cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,28 @@ var del        = require('del'),
     sourcemaps = require('gulp-sourcemaps'),
     sass       = require('gulp-sass'),
     imagemin   = require('gulp-imagemin'),
-    rename     = require('gulp-rename');
+    rename     = require('gulp-rename'),
+    connect    = require('gulp-connect'),
+    eslint     = require('gulp-eslint');
 
 
-gulp.task('scripts', ['clean'], () => {
+
+/**
+ * Lints every .js file inside the ./src/js folder, outputs
+ * the results and terminates if any errors are found
+ */
+gulp.task('eslint', () => {
+    return gulp.src('src/**/*.js')
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+/**
+ * Concatenates and minifies all the .js files inside ./src/js folder
+ * generates the source maps and copies everyting to ./dist folder
+ */
+gulp.task('scripts', ['eslint', 'clean'], () => {
     return gulp.src([
             'src/js/circle/autogrow.js',
             'src/js/circle/circle.js',
@@ -20,12 +38,17 @@ gulp.task('scripts', ['clean'], () => {
         .pipe(concat('all.min.js'))
         .pipe(uglify())
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('dist/scripts'));
+        .pipe(gulp.dest('dist/scripts'))
+        .pipe(connect.reload());
 });
 
 
+/**
+ * Compiles and minifies all the scss files inside ./src/sass folder
+ * generates the source maps and copies everyting to ./dist folder
+ */
 gulp.task('styles', ['clean'], () => {
-    return gulp.src('src/sass/global.scss')
+    return gulp.src('src/sass/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(rename('all.min.css'))
@@ -34,22 +57,56 @@ gulp.task('styles', ['clean'], () => {
 });
 
 
+/**
+ * Optimises all images inside ./src/images folder and
+ * copies them to the ./dist folder
+ */
 gulp.task('images', ['clean'], () => {
     return gulp.src('src/images/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('dist/images'))
+        .pipe(gulp.dest('dist/content'))
 });
 
 
+/**
+ * Deletes all files inside the ./dist folder
+ */
 gulp.task('clean', () => {
-    return del('dist');
+    return del('dist/**/*');
 });
 
 
+/**
+ * Builds the entire project by cleaning up the ./dist folder,
+ * calling the scripts, styles and images task and finally
+ * copying the icons subfolder
+ */
 gulp.task('build', ['scripts', 'styles', 'images'], () => {
     gulp.src('src/icons/**/*')
         .pipe(gulp.dest('dist/icons'));
 });
 
 
+/**
+ * Starts a web server for the index file inside the ./src folder
+ */
+gulp.task('serve', () => {
+    connect.server({
+      root: 'src',
+      livereload: true
+    });
+});
+
+
+/**
+ * Watches for changes in any .js files located inside the ./src/js folder
+ */
+gulp.task('watch', ['serve'], () => {
+    gulp.watch('src/js/**/*', ['scripts']);
+});
+
+
+/**
+ * Default gulp task runs the build command
+ */
 gulp.task('default', ['build']);
